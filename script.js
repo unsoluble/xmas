cd0371a8e6632b991c1a5c73f9245beff557cd2c
John Weston <unsoluble@me.com>
2023-11-29 14:36:53 -0800

    minimize

--- script.js
+++ script.js
@@ -1,51 +1,13 @@
-// script.js
-// Initial data
 const names = ["Megan", "Mike", "Jenny", "Mom", "John", "Mitts", "Alex", "Gino"];
-let rotationIndex = 5; // Adjusted rotationIndex to 5
-let currentYear = 2023;
+let rotationIndex = 5; // Starting index for 2023
 
-// Function to update the table based on the current year
-function updateTable(year) {
+function updateTable(rotationIndex) {
   for (let i = 0; i < names.length; i++) {
-    const currentName = names[i];
-    let assignedIndex;
-    let assignedName;
-    
-    // Repeat until currentName is not equal to assignedName
-    do {
-      assignedIndex = (rotationIndex + i) % names.length;
-      assignedName = names[assignedIndex];
-      rotationIndex = (rotationIndex + 1) % names.length;
-    } while (currentName === assignedName);
+    const assignedIndex = (rotationIndex + i) % names.length;
+    const assignedName = names[assignedIndex];
     
     document.getElementById(`assigned-to-${i}`).textContent = assignedName;
   }
-
-  // Update button onClick attributes
-  document.getElementById('prev-year').onclick = function() {
-    handleButtonClick(-1);
-  };
-
-  document.getElementById('next-year').onclick = function() {
-    handleButtonClick(1);
-  };
 }
 
-// Function to handle button click events and update the year
-function handleButtonClick(change) {
-  const newYear = currentYear + change;
-  setYear(newYear);
-}
-
-// Function to set the current year explicitly and update the table
-function setYear(year) {
-  const yearDifference = year - currentYear;
-  rotationIndex = (rotationIndex - yearDifference + names.length) % names.length;
-
-  currentYear = year;
-  document.getElementById('year').textContent = currentYear;
-  updateTable(currentYear);
-}
-
-// Initial table update
-updateTable(currentYear);
+updateTable(rotationIndex);
