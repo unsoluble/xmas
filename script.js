06292e70e7eae301c2b57e6db744d1e782011b66
John Weston <unsoluble@me.com>
2023-11-29 11:49:58 -0800

    avoid match

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let rotationIndex = 5; // Adjusted rotationIndex to 5
 let currentYear = 2023;
 
 // Function to update the table based on the current year
-function updateTable(year) {
+function updateTable(year, rotationIndex) {
   for (let i = 0; i < names.length; i++) {
     const currentName = names[i];
     const assignedIndex = (rotationIndex + i) % names.length;
@@ -33,11 +33,18 @@ function handleButtonClick(change) {
 // Function to set the current year explicitly and update the table
 function setYear(year) {
   const yearDifference = year - currentYear;
-  rotationIndex = (rotationIndex - yearDifference + names.length) % names.length;
+  let newRotationIndex = (rotationIndex - yearDifference + names.length) % names.length;
+
+  // Check for special cases where everyone is assigned their own self
+  if (year % names.length === 0) {
+    newRotationIndex = (newRotationIndex + 1) % names.length;
+  }
+
+  rotationIndex = newRotationIndex;
 
   currentYear = year;
   document.getElementById('year').textContent = currentYear;
-  updateTable(currentYear);
+  updateTable(currentYear, rotationIndex);
 }
 
 // Initial table update
